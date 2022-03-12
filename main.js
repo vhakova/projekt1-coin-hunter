5fa16c52b3b35f54f71f35e37d1edab3af3a843b
Veronika Háková <veronika.hakova@gmail.com>
2022-03-12 21:17:43 +0100

    uprava pisma

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-// toto budeš potřebovat později
-/*
-if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
-	// panacek a mince se prekryvaji
-}
-*/
-
-
 // sem začni psát svůj program
 
 
